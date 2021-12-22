1ad7de4f8c536f385c6bfdcbb78b0d380f362143
tgavrel <tomas.gavrel@hotmail.fr>
2021-12-22 17:12:18 +0100

    Updated bugs list

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const URL_API = "http://greenvelvet.alwaysdata.net/bugTracker/api"
+var userToken = sessionStorage.getItem("userToken");
+var userId = sessionStorage.getItem("userId");
 
 fetch(`${URL_API}/ping`)
     .then((res) => res.json())
@@ -7,12 +9,13 @@ fetch(`${URL_API}/ping`)
     })
     .catch((error) => console.error(error));
 
+
+
 $('#register').on('click', function(){
     var user = {
         user_name: $("#input_username").val(),
         password: $("#input_password").val()
     };
-    // console.log(user.user_name);
 
     $.ajax({
         type: 'GET',
@@ -28,6 +31,101 @@ $('#register').on('click', function(){
         }
     })
 })
+
+// Login user
+function login(){
+    var user = {
+        user_name: $("#input_username").val(),
+        password: $("#input_password").val()
+    };
+
+    $.ajax({
+        type: 'GET',
+        url: `${URL_API}/login/${user.user_name}/${user.password}`,
+        data: user,
+        dataType: "json",
+        success: function(data){
+            console.log('success', data.result);
+            sessionStorage.setItem("userToken", data.result.token);
+            sessionStorage.setItem("userId", data.result.id);
+            window.location.href="index.html"
+        },
+        error: function(error){
+            console.log('error login user', error);
+        }
+    })
+}
+
+// Logout user
+function logout(){
+    $.ajax({
+        type: 'GET',
+        url: `${URL_API}/logout/${userToken}`,
+        data: userToken,
+        dataType: "json",
+        success: function(data){
+            console.log('success', data.result);
+            sessionStorage.setItem("userToken", null);
+            window.location.href="login.html"
+        },
+        error: function(error){
+            console.log('error loggin-out user', error);
+        }
+    }) 
+}
+
+// Get all bugs list
+function getAllBugs(){
+    $.ajax({
+        type: 'GET',
+        url: `${URL_API}/list/${userToken}/0`,
+        data: {userToken, userId},
+        dataType: "json",
+        success: function(data){
+            console.log('success', 'all bugs list: ', data.result.bug);
+            data.result.bug.forEach(el => {
+                $('#bugsList')
+                .append(
+                    '<tr>'+
+                        '<th scope="row">'+
+                            `<span class="d-block">${el.title}</span>`+
+                            `<small>${el.description}</small>`+
+                        '</th>'+
+                        `<th class="text-center">${el.timestamp}</th>`+
+                        `<th class="text-center">${el.user_id}</th>`+
+                        '<th class="text-center">'+
+                        '<select class="form-select mx-auto" style="width: 150px;">'+
+                            `<option ${el.state == 0 ? 'selected' : ''} value="0">Non traité</option>`+
+                            `<option ${el.state == 1 ? 'selected' : ''} value="0">Non traité</option>`+
+                            `<option ${el.state == 2 ? 'selected' : ''} value="0">Non traité</option>`+
+                        '</select>'+
+                        '</th>'+
+                        '<th class="text-center">Supprimer</th>'+
+                    '</tr>'
+                );
+            });
+        },
+        error: function(error){
+            console.log('error saving user', error);
+        }
+    })
+}
+
+// Get user bugs list
+function getUserBugs(){
+    $.ajax({
+        type: 'GET',
+        url: `${URL_API}/list/${userToken}/${userId}`,
+        data: {userToken, userId},
+        dataType: "json",
+        success: function(data){
+            console.log('success', 'user bugs list: ', data.result.bug);
+        },
+        error: function(error){
+            console.log('error saving user', error);
+        }
+    })
+}
 
 
 // $(function (){
